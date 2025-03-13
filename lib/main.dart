7221ea09e58e003f50b3e5a57f0b85b0bd9dc0fa
MAULIZA APRILIA <118743543+MaulizaAprilia@users.noreply.github.com>
2025-03-13 22:28:36 +0700

    widget flutter

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'counter_app.dart';
+import 'layout_example.dart';
+import 'page_one.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainMenu(),
+    );
+  }
+}
+
+class MainMenu extends StatelessWidget {
+  const MainMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Main Menu')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const CounterApp()),
+                );
+              },
+              child: const Text('Go to Counter App'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LayoutExample()),
+                );
+              },
+              child: const Text('Go to Layout Example'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PageOne()),
+                );
+              },
+              child: const Text('Go to Page One'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
